1a1cab77890e873dd16e48199cf36e1d7313c4ec
zuo tai lian <antian_369@163.com>
2015-11-10 19:56:26 +0800

    修改上传文件保存

--- src/main/java/com/soa/service/busi/ldrk/UpdataLdrkZa.java
+++ src/main/java/com/soa/service/busi/ldrk/UpdataLdrkZa.java
@@ -57,28 +57,25 @@ public class UpdataLdrkZa extends BaseService {
         byte[] file = (byte[]) in.getObjectValue("ldrkdata");
         String name = in.getStringValue("ldrkdata_name");
         final String modul_name = "LDRKDATA";
-        String file_path = "";
         FileOutputStream fos;
         try {
-            file_path = sqjwUtil.upLoad(file, "zaldrk", modul_name, name);
+            final String file_path = sqjwUtil.upLoad(file, "zaldrk", modul_name, name);
+            /**
+             * 添加一个定时器的作用让写数据库写延迟两分钟执行
+             */
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        insertDb(file_path);
+                    } catch (IOException ex) {
+                        java.util.logging.Logger.getLogger(UpdataLdrkZa.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }).start();
         } catch (IOException ex) {
             java.util.logging.Logger.getLogger(UpdataLdrkZa.class.getName()).log(Level.SEVERE, null, ex);
         }
-        /**
-         * 添加一个定时器的作用让写数据库写延迟两分钟执行
-         */
-        new Timer().schedule(new TimerTask() {
-            
-            @Override
-            public void run() {
-                try {
-                    insertDb("D:\\test.txt");
-                } catch (IOException ex) {
-                    java.util.logging.Logger.getLogger(UpdataLdrkZa.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        }, 20000);
     }
 
     public void insertDb(String file_Path) throws IOException {
@@ -95,5 +92,4 @@ public class UpdataLdrkZa extends BaseService {
             update("add_ldrk_za", SystemUtil.getSerialNum(), data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24], data[25], data[26], data[27], data[28], data[29], data[30], data[31], data[32], data[33], data[34], data[35], data[36], data[37], data[38], data[39], data[40], data[41], data[42], data[43], data[44], data[45], data[46], data[47], data[48], data[49], data[50], data[51], data[52], data[53], data[54], data[55], data[56], pic_path);
         }
     }
-
 }
