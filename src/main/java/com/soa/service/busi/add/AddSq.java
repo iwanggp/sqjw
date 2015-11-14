cc592a90849cfb4b3fe985900a6cc25a173bb322
zuo tai lian <antian_369@163.com>
2015-11-14 10:23:32 +0800

    还原增加房屋

--- src/main/java/com/soa/service/busi/add/AddSq.java
+++ src/main/java/com/soa/service/busi/add/AddSq.java
@@ -10,11 +10,10 @@ import ch.qos.logback.classic.util.ContextInitializer;
 import com.lianzt.commondata.AbstractCommonData;
 import com.soa.exception.GlobalException;
 import com.soa.service.BaseService;
+import static com.soa.service.BaseService.getSession;
 import com.soa.util.SystemUtil;
 import com.soa.util.sqjw.SqjwUtil;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,38 +51,10 @@ public class AddSq extends BaseService {
         //in从页面传来过得值
         in.put("cjr", acd.get(SystemUtil.loginRemark));
         String sqid = SystemUtil.getSerialNum();
-        String sqmc = in.getStringValue("sqmc");
         in.putStringValue("sqid", sqid);//数据库的主码
         update("sq_add_sq", in);
-        List<AbstractCommonData> acdList = queryList("get_info_sqjz", sqmc);
-        /**
-         * 开始添加建筑信息的同时添加房屋信息
-         */
-        List<Object[]> jzArgsList = new ArrayList<Object[]>();
-        List<Object[]> fwArgsList = new ArrayList<Object[]>();
-        String dzbm = "";
-        String mlphm = "";
-        String jd = "";
-        String wd = "";
-        AbstractCommonData mph = null;
-        String mphm = "";
-        String dzxz="";
-        for (AbstractCommonData _acd : acdList) {
-            if (!_acd.isEmpty()) {
-                dzbm = _acd.getStringValue("jzdzbm");
-                dzxz=_acd.getStringValue("dzxz");
-                mlphm = _acd.getStringValue("mlphm");
-                jd = _acd.getStringValue("zbwzx");
-                wd = _acd.getStringValue("zbwzy");
-                mph = queryData("get_mph_jz", mlphm);
-                mphm = mph.getStringValue("mpmc");
-                jzArgsList.add(new Object[]{sqid, dzbm, _acd.getStringValue("dzxz"), _acd.getStringValue("dzxz"), _acd.getStringValue("jlxmc"), jd, wd, _acd.getStringValue("ssjwqdm"), _acd.getStringValue("dys"), _acd.getStringValue("hs"), _acd.getStringValue("lcs"), dzbm, mphm, _acd.getDateValue("cjrq"), _acd.getStringValue("cjry"), _acd.getStringValue("jlxdm"), _acd.getStringValue("mlphm")});//自动添加该社区内的所有建筑信息#servic_code:P24001
-                fwArgsList.add(new Object[]{mphm, dzxz,jd, wd, dzbm, sqid, dzbm});
-            }
-        }
-        batchUpdate("autoadd_sq_jz", jzArgsList);
-        batchUpdate("autoadd_sq_jz_zjh", fwArgsList);
-//        update("autoadd_sq_jz_zjh", mphm, jd, wd, dzbm, sqid, dzbm);//自动添加该社区内的所有建筑信息#servic_code:P24002
+        update("autoadd_sq_jz", sqid, in.getStringValue("sqmc"));//自动添加该社区内的所有建筑信息
+        update("autoadd_sq_jz_zjh", sqid, in.getStringValue("sqmc"));//向建筑内添加相应的服务信息
 
     }
 }
